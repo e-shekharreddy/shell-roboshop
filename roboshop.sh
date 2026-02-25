81a601b4a1110e1150a0dbaf680348c45ca8839d
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-02-25 23:25:22 +0530

    updated

--- roboshop.sh
+++ roboshop.sh
@@ -12,7 +12,7 @@ do
     --security-group-ids $SG_ID \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
     --query 'Instances[0].InstanceId' \
-    --output text \ )
+    --output text )
 
     aws ec2 wait sleep 5 instance-running --instance-ids "$INSTANCE_ID"
 
@@ -21,14 +21,14 @@ do
             aws ec2 describe-instances \
             --instance-ids $INSTANCE_ID \
             --query 'Reservations[].Instances[].PublicIpAddress' \
-            --output text \
+            --output text
             )
     else
         IP=$( 
             aws ec2 describe-instances \
             --instance-ids $INSTANCE_ID \
             --query 'Reservations[].Instances[].PrivateIpAddress' \
-            --output text \
+            --output text
             )
         fi
 

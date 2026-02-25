d0818826a882ddd29ccc534de25895b04e6231ff
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-02-26 01:40:28 +0530

    updated

---
+++ mongodb.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop" # full path
+LOGS_FILE="/var/log/shell-roboshop/$0.log" # or we can write it as $LOGS_FOLODER/$0.log
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]; then
+    echo -e " $R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
+fi 
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2... $R FAILURE $N " | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$2... $G SUCCESS $N " | tee -a $LOGS_FILE
+    fi
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Copying Mongo Repo"
+
+dnf install mongodb-org -y 
+VALIDATE $? "Instaling MongoDB server"
+
+systemctl enable mongod 
+VALIDATE $? "Enable MongoDB"
+
+systemctl start mongod 
+VALIDATE $? "start MongoDB"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Allowing remote connections"
+
+systemctl restart mongod
+VALIDATE $? "Restarted MongoDB"
+
+
+

ff2e7f3f08ec2859d3aff5cf9587de682074ed5b
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-02-27 14:01:23 +0530

    updated

--- 04-redis.sh
+++ 04-redis.sh
@@ -12,6 +12,7 @@ N="\e[0m"
 
 SCRIPT_DIR=$PWD
 MONGODB_HOST="mongodb.tsmvr.fun"
+REDIS_HOST="redis.tsmvr.fun"
 
 if [ $USERID -ne 0 ]; then
     echo -e " $R Please run this script with root user access $N" | tee -a $LOGS_FILE

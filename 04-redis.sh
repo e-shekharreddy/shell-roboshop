36a1f4dd943d9226a1a515c1c8f8c9bb55ac2119
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-02-27 12:58:43 +0530

    updated

---
+++ 04-redis.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop" # full path
+LOGS_FILE="/var/log/shell-roboshop/$0.log" # or we can write it as $LOGS_FOLODER/$0.log
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m" 
+N="\e[0m"
+
+SCRIPT_DIR=$PWD
+MONGODB_HOST="mongodb.tsmvr.fun"
+
+if [ $USERID -ne 0 ]; then
+    echo -e " $R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
+fi 
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2... $R FAILURE $N " | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$2... $G SUCCESS $N " | tee -a $LOGS_FILE
+    fi
+}
+
+dnf module disable redis -y &>>$LOGS_FILE
+VALIDATE $? "Disable redis default version"
+
+dnf module enable redis:7 -y &>>$LOGS_FILE
+VALIDATE $? "Enableing redis:7"
+
+dnf install redis -y &>>$LOGS_FILE
+VALIDATE $? "Installing redis"
+
+sed -i -e 's/1270.0.0.1/0.0.0.0/g' -e  '/protected-mode/ c protected-mode no' /etc/redis/redis.conf 
+VALIDATE $? "updating changes allowinfg remote connections"
+
+systemctl enable redis 
+systemctl start redis &>>$LOGS_FILE
+VALIDATE $? "Enable and started redis"
